ffdcc6eeefaa1f909f765c073efc1107d62ae10d
shaikhshehzad <shehzadali3113@gmail.com>
2023-01-09 14:10:25 +0530

    drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drag-and-drop';
+
+  public listofElements = [1,23,34,5,6,67,7,88,4,5,6,77,3,23,3,4,5,234,6] ;
+
+  movies = [
+    'Episode I - The Phantom Menace',
+    'Episode II - Attack of the Clones',
+    'Episode III - Revenge of the Sith',
+    'Episode IV - A New Hope',
+    'Episode V - The Empire Strikes Back',
+    'Episode VI - Return of the Jedi',
+    'Episode VII - The Force Awakens',
+    'Episode VIII - The Last Jedi',
+    'Episode IX – The Rise of Skywalker',
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    // then below line will get you the event it has properties like previous and current index
+    console.log("-------------event-----below-------")
+    console.log(event)
+    
+  }
+
+
 }
